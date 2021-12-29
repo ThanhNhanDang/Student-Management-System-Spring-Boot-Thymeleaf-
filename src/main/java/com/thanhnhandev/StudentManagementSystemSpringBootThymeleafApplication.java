e3d962b2655a7b27f5071ea6631d8eabf702e866
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-12-29 16:59:37 +0700

    first commit

---
+++ src/main/java/com/thanhnhandev/StudentManagementSystemSpringBootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.thanhnhandev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemSpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemSpringBootThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanhnhandev/StudentManagementSystemSpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhnhandev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemSpringBootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
